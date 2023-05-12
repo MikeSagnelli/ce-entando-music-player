47657babb3ae409e667378dbe284beb0f70b6afb
MikeSagnelli <ing.sagnelli@gmail.com>
2023-05-12 16:55:06 +0200

    feat: setup api communication

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { createGlobalStyle } from 'styled-components'
 import { normalize } from 'styled-normalize'
 
 import { DARK_THEME, LIGHT_THEME } from './utils/theme'
 import { ComponentsProvider } from './components/ComponentsProvider'
+import { getSoundInformation, searchSounds } from './api'
 
 const GlobalStyle = createGlobalStyle`
   ${normalize}
@@ -17,6 +18,16 @@ const GlobalStyle = createGlobalStyle`
 function App() {
   const [isDarkTheme, setIsDarkTheme] = useState(false)
   const theme = isDarkTheme ? DARK_THEME : LIGHT_THEME
+
+  const testFreesoundAPI = async () => {
+    const response1 = await searchSounds('dogs')
+    const response2 = await getSoundInformation(response1?.results?.[0]?.id)
+    console.log({ response1, response2 })
+  }
+
+  useEffect(() => {
+    testFreesoundAPI()
+  }, [])
 
   console.log('TEMP: ', setIsDarkTheme)
 
